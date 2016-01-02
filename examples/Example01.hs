371ee315cd06e87d926d0faf2d8c4a075b7870ad
Halvor Granskogen Bjornstad <halvorgb@gmail.com>
2016-01-02 13:46:56 +0100

    moved eventHandler to client, initial work on animated sprite support.

--- examples/Example01.hs
+++ examples/Example01.hs
@@ -6,6 +6,8 @@ import qualified Data.Map      as M
 import qualified Data.Text     as T
 import qualified Data.Word     as W
 import           SDL.Event
+import           SDL.Input
+import Control.Monad
 import           System.Random
 
 config = BallastConfig (640, 480) "Hello World"
@@ -16,13 +18,24 @@ data Universe =
            }
 
 instance Cargo Universe where
-  spriteMap u = M.empty
+  renderables universe = []
 
-  updateFunction universe events = universe
+  eventFunction universe events = foldM handleEvent universe events
+    where
+      handleEvent :: Universe -> Event -> Maybe Universe
+      handleEvent universe (Event _ (KeyboardEvent keyboardEvent))
+        | keyboardEventKeyMotion keyboardEvent == Pressed &&
+          keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ =
+            Nothing
+        | otherwise =
+          Just universe
+
+      handleEvent universe _ = Just universe
 
   updateFunction universe delta
     | something universe == "somethang" = universe
     | otherwise = universe
+
 
 initialSeed = 1
 initialUniverse = Universe "something" $ mkStdGen initialSeed
--- src/Ballast.hs
+++ src/Ballast.hs
@@ -4,18 +4,32 @@ import           Control.Monad (unless)
 import qualified Data.Map      as M
 import qualified Data.Text     as T
 import qualified Data.Word     as W
-import           Linear        (V4 (..))
+import           Linear
+import           Linear.Affine
 import           SDL
 import           SDL.Time
 
 class Cargo a where
-  spriteMap :: a -> M.Map String Surface
+  renderables :: a -> [Renderable]
   eventFunction ::  a -> [Event] -> Maybe a
   updateFunction :: a -> W.Word32 -> a
 
+
+data Renderable =
+  Renderable { rPosition             :: V2 Int
+             , rDimensions           :: V2 Int
+             , rTexture              :: Texture
+             , rRotation             :: Int -- degrees
+             , rSpritePosition       :: V2 Int
+             , rSpriteDimensions     :: V2 Int
+             , rSpriteSize           :: V2 Int
+             , rNofFrames            :: Int -- number of frames in the texture
+             , rMillisecondsPerFrame :: Int
+             }
+
 data BallastConfig =
-  BallastConfig { dimensions :: (Int, Int)
-                , title      :: T.Text
+  BallastConfig { bcDimensions :: (Int, Int)
+                , bcTitle      :: T.Text
                 }
 
 
@@ -26,22 +40,21 @@ millisecondsPerUpdate = 16
 run :: Cargo c => BallastConfig -> c -> IO ()
 run bc cargo = do
   initializeAll
-  window <- createWindow (title bc) defaultWindow
+  window <- createWindow (bcTitle bc) defaultWindow
   renderer <- createRenderer window (-1) defaultRenderer
 
-  ts <- ticks
-  let lag = 0
-  gameLoop renderer bc cargo ts lag
-
+  initialTick <- ticks
+  let initialLag = 0
+  gameLoop renderer bc cargo initialTick initialLag
   -- do we need to deinitialize?
 
 -- | Gameloop: http://gameprogrammingpatterns.com/game-loop.html'
 --  ticks = milliseconds since initialization, elapedTicks is the delay (delta) inbetween each iteration.
 gameLoop :: Cargo c => Renderer -> BallastConfig -> c -> W.Word32 -> W.Word32 -> IO ()
-gameLoop renderer bc previousCargo previousTicks previousLag = do
+gameLoop renderer bc previousCargo previousTick previousLag = do
   -- Poll ticks.
-  currentTicks <- ticks
-  let elapsedTicks = currentTicks - previousTicks
+  currentTick <- ticks
+  let elapsedTicks = currentTick - previousTick
 
   -- poll and handle events
   events <- pollEvents
@@ -51,24 +64,13 @@ gameLoop renderer bc previousCargo previousTicks previousLag = do
     Just handledEventsCargo -> do
       -- update
       let (updatedCargo, remainingLag) = update previousCargo $ previousLag + elapsedTicks
- --updateWhileLagging previousCargo $ previousLag + elapsedTicks
 
+      print remainingLag
       -- render
-      render renderer bc updatedCargo
+      render renderer bc updatedCargo currentTick
 
       -- loop
-      gameLoop renderer bc updatedCargo currentTicks remainingLag
-
-  -- where
-  --   eventIsQPress event =
-  --     case eventPayload event of
-  --       KeyboardEvent keyboardEvent ->
-  --         keyboardEventKeyMotion keyboardEvent == Pressed &&
-  --         keysymKeycode (keyboardEventKeysym keyboardEvent) == KeycodeQ
-  --       _ -> False
-
-  --   qPressed events = not (null (filter eventIsQPress events))
-
+      gameLoop renderer bc updatedCargo currentTick remainingLag
 
 
 handleEvents :: Cargo c => c -> [Event] -> Maybe c
@@ -80,13 +82,31 @@ update cargo lag = updateWhileLagging cargo lag
     updateWhileLagging :: Cargo c => c -> W.Word32 -> (c, W.Word32)
     updateWhileLagging cargo lag
       | lag >= millisecondsPerUpdate =
-        updateWhileLagging cargo $ lag - millisecondsPerUpdate
-
+        let updatedCargo = updateFunction cargo $ millisecondsPerUpdate
+        in updateWhileLagging updatedCargo $ lag - millisecondsPerUpdate
       | otherwise =
         (cargo, lag)
 
-render :: Cargo c => Renderer -> BallastConfig -> c -> IO ()
-render renderer cargo bc = do
+
+render :: Cargo c => Renderer -> BallastConfig -> c -> W.Word32 -> IO ()
+render renderer bc cargo currentTick = do
   rendererDrawColor renderer $= V4 0 0 255 255
   clear renderer
+  mapM_ (renderRenderable renderer currentTick) $ renderables cargo
   present renderer
+
+renderRenderable :: Renderer -> W.Word32 -> Renderable -> IO ()
+renderRenderable renderer currentTick renderable =
+  copy renderer t (Just from) (Just to)
+  where
+    t = rTexture renderable
+    frame = (fromIntegral currentTick)
+            `div` (rMillisecondsPerFrame renderable)
+            `mod` (rNofFrames renderable)
+
+    (V2 spriteWidth _) = rSpriteDimensions renderable
+    spritePositionOffset = fmap fromIntegral
+                           $ V2 (spriteWidth * frame) 1
+                           * (rSpritePosition renderable)
+    from = Rectangle (P spritePositionOffset) $ fmap fromIntegral $ rDimensions renderable
+    to   = Rectangle (P $ fmap fromIntegral (rPosition renderable)) $ fmap fromIntegral $ rDimensions renderable
